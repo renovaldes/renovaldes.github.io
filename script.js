96e3f86d75bd91dcaa8256ed5c565c6f37237a81
Reno  Valdes <renovaldes27@gmail.com>
2016-12-22 12:34:36 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 $(function(){
    $('#butt').on('click', function(){
-      $('#quote').hide();
+      $.getJSON("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=", function(a) {
+         $("body").append(a[0].content + "<p>— " + a[0].title + "</p>")
+      });
    });
   
 });
