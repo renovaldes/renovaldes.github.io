83f93d88128e7478176678bba23fec97a9cef64b
Reno  Valdes <renovaldes27@gmail.com>
2016-12-21 18:44:20 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 $(function(){
+   $('#butt').on('click', function(e) {
+    e.preventDefault();
+    $.ajax( {
+      url: '/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1',
+      success: function(data) {
+        var post = data.shift(); // The data is an array of posts. Grab the first one.
+        //$('#quote-title').text(post.title);
+        $('#quote').html(post.content);
+      },
+      cache: false
+    });
+  });
+  
+  
 });
