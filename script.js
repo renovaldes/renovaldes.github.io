0b5c005d264606973b80268c67ba0b097598eb5b
Reno  Valdes <renovaldes27@gmail.com>
2016-12-22 19:13:20 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(function(){
-   var url = http://api.forismatic.com/api/1.0/;
+   var url = api.forismatic.com/api/1.0/;
    
    var request = 'method=getQuote&format=json&lang=en';
 	ajax(url, request, function(data){
