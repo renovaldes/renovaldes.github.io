7214669672c25f6fb24c11c6f67c61ecf984da6e
Reno  Valdes <renovaldes27@gmail.com>
2016-12-23 16:11:29 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,9 @@ $(function(){
 	var api = 'http://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=?';
 	
 	$('#butt').on('click', function(){
-		$.getJSON(api)
-	.success(function(data){
-		$('#quote').html(""+data.quoteText);
-		$('#author').html("-"+data.quoteAuthor);
+		$.getJSON(api).success(function(data){
+			$('#quote').html(""+data.quoteText);
+			$('#author').html("-"+data.quoteAuthor);
 		});
 	});
 });
