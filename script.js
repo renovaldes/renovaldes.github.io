9bdbbc439c316c51bcc6460e15d4de7aa06b864c
Reno  Valdes <renovaldes27@gmail.com>
2016-12-22 19:11:56 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 $(function(){
-   $('#butt').on('click', function(){
-      $.getJSON("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=", function(a) {
-         $("#quote").html(a[0].content);
-      });
+   var url = http://api.forismatic.com/api/1.0/;
+   
+   var request = 'method=getQuote&format=json&lang=en';
+	ajax(url, request, function(data){
+   
+      $('#quote').html(data);
+   
    });
   
 });
